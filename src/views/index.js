4f4a8fa02c163d9af5191191708c90d13d4ce9aa
vivek6159 <48536022+vrathod3010@users.noreply.github.com>
2019-09-23 13:01:49 +1000

    updates

---
+++ src/views/index.js
@@ -0,0 +1,15 @@
+/***
+ *  Created by Sanchit Dang
+ ***/
+import { Login } from './common/Login/Login';
+import { Register } from './common/Register/Register'
+import { Home } from './dependants/Home/Home'
+import { User } from './dependants/User/User'
+import { PatientContainer } from './dependants/Patient/PatientContainer'
+export {
+    Login,
+    Register,
+    Home,
+    User,
+    PatientContainer,
+}

1e351c0aab75335b259704fd44544abd9f353b36
linhtang90 <xltang@edu.deakin.au>
2019-10-02 21:13:41 +1000

    Modify the backend database link

--- src/helpers/axiosInstance.js
+++ src/helpers/axiosInstance.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 //const baseURL = process.env.REACT_APP_BASE_URL + '/api/';
 
-  const baseURL = 'http://launchpad-heart-lb.herokuapp.com' + '/api/';
+  const baseURL = 'https://launchpad-heart-lb.herokuapp.com' + '/api/';
 
 export const axiosInstance = axios.create({
   baseURL: baseURL,
